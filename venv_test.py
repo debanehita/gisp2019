fcdc100e0e2526cbd5da6274200f3946611e0586
Mark Foley <mark@markfoley.info>
2019-01-29 13:21:38 +0000

    latest

---
+++ venv_test.py
@@ -0,0 +1,21 @@
+"""
+You can use this file to test if your standard GIS-related imports work
+"""
+
+try:
+    import gdal_workaround
+    import turtle as t
+    from shapely.geometry import mapping
+    from shapely.wkt import loads
+    import psycopg2
+    import psycopg2.extras
+    import fiona
+    from fiona.crs import from_epsg
+    import pyproj
+    from osgeo import ogr
+    from osgeo import gdal
+
+    print("Didn't see any import errors")
+
+except Exception as e:
+    print("{}".format(e))
